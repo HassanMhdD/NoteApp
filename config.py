0e4305fe1379972488d457a2762e61e797384f19
MOHAMED DAOUD <hassan-mahad.mohamed-daoud@capgemini.com>
2023-03-21 10:35:47 +0100

    postres db connected

---
+++ config.py
@@ -0,0 +1,6 @@
+postgresql = { 'hostname': 'localhost',
+               'database': 'Employee',
+               'username': 'postgres',
+               'pwd':'hassan',
+               'port_id': 5432
+    }
